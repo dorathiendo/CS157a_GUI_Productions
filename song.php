b5b3044a624bea87bfc909a2d780a0ebbf68db0a
rominoushana <rominoushana@gmail.com>
2014-12-04 21:38:42 -0800

    Added song page and like/dislike bar

--- song.php
+++ song.php
@@ -66,60 +66,69 @@ th    {
 td 	   {
 		font-size:24px;
 		}
+		
+#likes{
+	height:5px;
+	width:50%;
+	background:#00FF00;
+	float:left;
+}
+#dislikes{
+	height:5px;
+	width:50%;
+	background:#FF0000;
+	float:right;
+}
+
+#bar{
+	width:400px;
+}
+
+#all{
+float:right;
+margin-right:100px;
+margin-bottom: 20px;
+}
+
+}
 </style>
 </head>
 
-<body>
-<div align="center">
+<body onload="activate()">
+<div align="center"> 
      <form action="main.php" method="post"> 
      	 <input name="submit" type="text" id="search"/>
         <input type="submit" id="unibutton" value="Search"/>
      </form> 
- </div> <br />	
- <div align="center"> 
- 	<form id="search" action="main.php" method="post">
-    Categories:
-  		 	<input type="submit" name="submit" id="unibutton"  value="Pop"/>
-  			<input type="submit" name="submit" id="unibutton"  value="Hip-Hop"/>
-  			<input type="submit" name="submit" id="unibutton"  value="Alternative">
-  			<input type="submit" name="submit" id="unibutton"  value="Country"/>
-  			<input type="submit" name="submit" id="unibutton"  value="Indie"/>
-  			<input type="submit" name="submit" id="unibutton"  value="Electronics"/>
-            <input type="submit" name="submit" id="unibutton"  value="Club"/>
-    </form>
- </div>
-<br /> 
+ </div> <br />
 
-
-
- <?php
-	
-	$searchby = null;
-		
-	if(isset($_POST['submit'])) 
-	{
-		$searchby = $_POST['submit']; 
-	}
-	
-	$servername = "localhost";
+<div id="all">
+	<div id = "likebuttons" class="inner">
+	<input type="button" value="Like" id="likeButton" onclick="like();"/>
+	<input type="button" value="Dislike" id="dislikeButton" onclick="dislike();"/>
+	</div>
+	<div id="bar" class="inner">
+		<div id="likes"></div>
+		<div id="dislikes"></div>
+	</div>
+</div>
+</body>
+ 
+  <?php
+  	$servername = "localhost";
 	$serverusername = "root";
 	$password = "";
 	$dbname = "music library";
-
-// Create connection
+ // Create connection
 $conn = new mysqli($servername, $serverusername, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
      die("Connection failed: " . $conn->connect_error);
 } 
 
-
-$sql = "SELECT * FROM Song INNER JOIN Likes ON Song.songID = Likes.songID";
-
-
-if($searchby == "Pop" || $searchby == "Hip-Hop" || $searchby == "Alternative" || $searchby == "Country" || $searchby == "Indie" || $searchby == "Electronic" || $searchby == "Club" ) 
-{
-	$sql = "SELECT * FROM song INNER JOIN Likes ON Song.songID = Likes.songID WHERE Genre = '$searchby'"; 	
+if(isset($_GET["songID"])) {
+		$song_id = $_GET["songID"];
+$sql = "SELECT * FROM Song INNER JOIN Likes ON Song.songID = Likes.songID WHERE Song.songID = '".$song_id."'";
 }
 else if(isset($_POST['submit']))
 {
@@ -130,6 +139,7 @@ else if(isset($_POST['submit']))
 }
 
 $result = $conn->query($sql);
+
 
 if ($result->num_rows > 0) {
      echo "<table align='center'>
@@ -144,14 +154,13 @@ if ($result->num_rows > 0) {
 					<th>Dislikes</th>
 				</tr>";
      // output data of each row
-	 $row =$result->fetch_assoc();
-	 
     while($row = $result->fetch_assoc())
 	 {
-	 $idOfSong=$row["songID"];
+	 $likes=$row["noOfLikes"];
+	 $dislikes=$row["noOfDislikes"];
       echo "<tr align='center'> 
 		  <td> <img width='150px' height='150px' src='" . $row["AlbumCover"]. " '/> </td> 
-		  <td><a href = 'http://localhost/ml/song.php?songID=".$idOfSong."'>" .$row["Title"]. " </a></td>
+		  <td>" .$row["Title"]. " </td>
 		  <td>" .$row["Artists"]."</td>
 		  <td>".$row["Length"]." </td>
 		  <td> ".$row["Genre"]."</td> 
@@ -167,7 +176,46 @@ else
 	echo "result is 0" ; 
 
 ?>
-
 </body>
+<script type="text/javascript">
+var likes="<?php echo $likes; ?>", 
 
+dislikes="<?php echo $dislikes; ?>";
+
+//Functions to increase likes and immediately calculate bar widths
+function like(){
+	likes++;
+	calculateBar();
+}
+function dislike(){
+	dislikes++;
+	calculateBar();
+}
+
+function activate()
+{
+like();
+likes--;
+dislike();
+dislikes--;
+}
+//Calculates bar widths
+function calculateBar(){
+	var total= likes+dislikes;
+    //Simple math to calculate percentages
+	var percentageLikes=(likes/total)*100;
+	var percentageDislikes=(dislikes/total)*100;
+
+    //We need to apply the widths to our elements
+	document.getElementById('likes').style.width=percentageLikes.toString()+"%";
+	document.getElementById('dislikes').style.width=percentageDislikes.toString()+"%";
+    
+    //We add the numbers on the buttons, just to show how to
+    document.getElementById('likeButton').value="Likes ("+likes.toString()+")";
+    document.getElementById('dislikeButton').value="Disikes ("+dislikes.toString()+")";
+
+}
+
+calculateBar();
+</script>
 </html>
